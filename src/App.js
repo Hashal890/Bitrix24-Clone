a44d3f69a57f663249af98cd9e21e418f990ef48
moneyforey <moneyforey786@gmail.com>
2022-09-28 23:19:09 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Login from "./Routes/Login";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Betrix24 Clone</h1>
+     <Login/>
     </div>
   );
 }
