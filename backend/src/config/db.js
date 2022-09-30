28abc5c16c4412a8c5b862d906654708e09ecc9d
vishalmahle11 <vishalmahale974@gmail.com>
2022-09-30 18:14:05 +0530

    Complete Todo API

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,20 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   console.log("mongo connect");
-  return mongoose.connect("mongodb://127.0.0.1:27017/bitrix24");
+  return mongoose.connect(
+    "mongodb+srv://vishalmahale11:js864BTBC24fWlwZ@cluster0.tktvy3k.mongodb.net/bitrixstack?retryWrites=true&w=majority"
+  );
 };
 
 module.exports = connect;
+
+// const mongoose = require("mongoose");
+
+// const connect = () => {
+//   console.log("mongo connect");
+//   return mongoose.connect(
+//     "mongodb://127.0.0.1:27017/bitrix"
+//   );
+// };
+
+// module.exports = connect;
